58efa41dec7ba6d7a4a447521e78b8fea20b62d5
Ashmit Singh <connect.ashmit.singh@proton.me>
2026-02-26 01:39:12 +0530

    UC8: Linked List Based Palindrome Checker

---
+++ UC8_LinkedListBased.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+
+/**
+ * ==================================================
+ * MAIN CLASS - UC8_LinkedListBased
+ * ==================================================
+ * Use Case 8: Linked List Based Palindrome Checker
+ *
+ * Description:
+ * Uses LinkedList removeFirst/removeLast for
+ * symmetric validation.
+ *
+ * @author Developer
+ * @version 8.0
+ *          ==================================================
+ */
+public class UC8_LinkedListBased {
+    public static void main(String[] args) {
+        String input = "level";
+        LinkedList<Character> list = new LinkedList<>();
+        for (char c : input.toCharArray()) {
+            list.add(c);
+        }
+        boolean isPalindrome = true;
+        while (list.size() > 1) {
+            char first = list.removeFirst();
+            char last = list.removeLast();
+            if (first != last) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
